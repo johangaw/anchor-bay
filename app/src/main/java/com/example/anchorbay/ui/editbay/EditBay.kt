24b681c1ad881f8bdb1504dd56ad6a6878707c66
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2021-03-11 15:58:53 +0100

    Completed RatingBar comp

--- app/src/main/java/com/example/anchorbay/ui/editbay/EditBay.kt
+++ app/src/main/java/com/example/anchorbay/ui/editbay/EditBay.kt
@@ -1,26 +1,16 @@
-package com.example.anchorbay.ui
+package com.example.anchorbay.ui.editbay
 
-import android.widget.ImageButton
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.LocationOn
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -29,16 +19,19 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.anchorbay.R
-import com.example.anchorbay.ui.editbay.LabelSelect
 
 @Composable
-fun RatingBar() {
+fun RatingBar(value: Int, onValueChange: (value: Int) -> Unit) {
     Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
         repeat(5) {
+            val selected = it < value
+            val color by animateColorAsState(targetValue = if (selected) Color.Yellow else Color.Black)
             Image(
-                modifier = Modifier.size(50.dp),
-                painter = painterResource(R.drawable.ic_baseline_star_outline_24),
-                colorFilter = ColorFilter.tint(color = Color.Black),
+                modifier = Modifier
+                    .clickable { onValueChange(it + 1) }
+                    .size(50.dp),
+                painter = painterResource(if (selected) R.drawable.star_filled else R.drawable.start_unfilled),
+                colorFilter = ColorFilter.tint(color = color),
                 contentDescription = null
             )
         }
@@ -48,6 +41,8 @@ fun RatingBar() {
 
 @Composable
 fun EditBay() {
+    var rating by remember { mutableStateOf(0) }
+
     Column(Modifier.verticalScroll(rememberScrollState())) {
         Image(
             painter = painterResource(R.drawable.hero),
@@ -58,12 +53,12 @@ fun EditBay() {
                 .height(250.dp)
         )
         Column(Modifier.padding(16.dp)) {
-            RatingBar()
+            RatingBar(rating) { rating = it }
             OutlinedTextField(value = "", onValueChange = { }, modifier = Modifier.fillMaxWidth())
             Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-               IconButton(onClick = {}) {
-                   Icon(Icons.Rounded.LocationOn, contentDescription = "")
-               }
+                IconButton(onClick = {}) {
+                    Icon(Icons.Rounded.LocationOn, contentDescription = "")
+                }
                 IconButton(onClick = {}) {
                     Text(text = "NW", style = MaterialTheme.typography.h5)
                 }
@@ -71,8 +66,13 @@ fun EditBay() {
             LabelSelect()
             BoatSelect()
 
-            var str by remember { mutableStateOf("")}
-            TextField(value = str, onValueChange = { str = it }, Modifier.fillMaxWidth(), singleLine = false,)
+            var str by remember { mutableStateOf("") }
+            TextField(
+                value = str,
+                onValueChange = { str = it },
+                Modifier.fillMaxWidth(),
+                singleLine = false,
+            )
         }
     }
 }
@@ -91,7 +91,6 @@ fun BoatSelect() {
         }
     }
 }
-
 
 
 @Composable
