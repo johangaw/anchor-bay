cc50ce66b11a92919997c502de33a35f4b03a317
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2021-03-16 17:40:53 +0100

    Stuff works on latest and greatest

--- app/src/main/java/com/example/anchorbay/MainActivity.kt
+++ app/src/main/java/com/example/anchorbay/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.anchorbay
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.anchorbay.ui.editbay.EditBay
 import com.example.anchorbay.ui.theme.AnchorBayTheme
 
 class MainActivity : AppCompatActivity() {
+
+    @ExperimentalMaterialApi
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AnchorBayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    EditBay()
                 }
             }
         }
